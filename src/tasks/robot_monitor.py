c5ce9f55813cb8941b12dfc3cf221f128a6a2630
lnalice <rara9915@gmail.com>
2024-12-17 00:57:49 +0900

    Fix : Resolve topic collision for multiple robots

--- src/tasks/robot_monitor.py
+++ src/tasks/robot_monitor.py
@@ -1,22 +1,38 @@
 import rospy
 import smach
-import smach_ros
 
 from std_msgs.msg import String
 
-def monitor_cb(user_data, res_msg):
-    result = str(res_msg.data).split() #{robot} {status} (ex. tb3_0 IDLE)
-
-    robot, status = result
-
-    if status == 'IDLE':
-        user_data.idle_robots.add(robot)
-    else:
-        user_data.idle_robots.discard(robot)
+# queue size를 정의할 수 있는 custome MonitorState
+class RobotMonitor(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['received', 'none'],
+                            io_keys=['idle_robots'])
+                             
+        self.res_data = None
     
-    rospy.loginfo(f"Robot {robot}'s status is updated to {status}.")
+    def monitor_cb(self, res_msg):
+        self.res_data = res_msg.data
 
-    return True
+    def execute(self, user_data):
+        rospy.Subscriber("/task_scheduler/robot_status", String, self.monitor_cb, queue_size=10)
+        rospy.sleep(1)
+
+        while True:
+            if self.res_data:
+                result = str(self.res_data).split() # {robot} {status} (ex. tb3_0 IDLE)
+                robot, status = result
+
+                if status == 'IDLE':
+                    user_data.idle_robots.add(robot)
+                else:
+                    user_data.idle_robots.discard(robot)
+                
+                rospy.loginfo(f"[RobotMonitor] Robot {robot}'s status is updated to {status}.")
+
+                self.res_data = None
+                return 'received'
+    
 
 class RobotMonitorSM(smach.StateMachine):
     def __init__(self):
@@ -24,12 +40,7 @@ class RobotMonitorSM(smach.StateMachine):
                              input_keys=['idle_robots'],
                              output_keys=['idle_robots'])
         
-        self.monitor = smach_ros.MonitorState("/task_scheduler/robot_status", String, monitor_cb,)
-
         with self:
-            self.add('INIT', self.monitor,
-                     transitions = {'done': 'MONITOR'})
-            self.add('MONITOR', self.monitor,
-                     transitions = {'invalid': 'MONITOR',
-                                   'valid': 'MONITOR',
-                                   'preempted': 'MONITOR'})
+            self.add('MONITOR', RobotMonitor(),
+                     transitions = {'received': 'MONITOR',
+                                   'none': 'MONITOR'})
